a05e7a4ef7303875b61b8d065d5d3fe183bdee55
abhilaad <71024710+abhilaad@users.noreply.github.com>
2021-08-19 03:07:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,83 @@ import '../styles/App.css';
 
 const App = () => {
   // write your code here 
+  let [counter, setCount] = useState(0);
+ 
+  let yo;
+  let z = 0;
+  let interval;
+ 
+  let y = () =>{
+   
+    interval = setInterval(() => {
+      console.log(counter + "inside");
+      
+      setCount(counter--);
+        
+      if (counter < 0 || counter == "NaN") {
+        clearInterval(interval);
+        
+      }
+    }, 1000);
+    console.log(interval + "  yuuooiii")
+    
+    
+  }
+
+            
+      useEffect(
+        ()=>{
+          
+          console.log("effected")
+        return () => {clearInterval(interval); };
+        },[interval]
+      )
+     
+    
+    
+ 
+  const onKeyPressed = (e)=> {
+    
+     yo = Number(e.key);
+     if(typeof(yo)=="number" && !Number.isNaN(yo)){
+      setCount(counter= counter?counter.toString()+yo.toString() : yo);
+
+      
+      console.log(counter);  
+       
+
+       }
+       if(e.key=="Enter"){
+         
+         y()
+       
+       }
+
+     else console.log("elseeee");
+     
+
+     } 
+     let changes = (e)=>{
+      setCount(e.target.value); 
+    
+      
+    
+  }
+  
 
   return (
-    <div className="wrapper">
+    <div className="wrapper">   HI
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={onKeyPressed} onChange={changes} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{counter?counter:z}</div>
     </div>
   )
 }
 
-
 export default App;
+
+
+
